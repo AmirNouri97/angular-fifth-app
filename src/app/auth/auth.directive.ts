be80399e0de9db3a921fbeea0d7fd2edb4d79f6b
Amirhossein Noori <nouri97dev@gmail.com>
2025-09-27 17:58:09 +0330

    feat: show or dont show the contect without *

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Permission } from './auth.model';
 
@@ -9,14 +9,18 @@ import { Permission } from './auth.model';
 export class AuthDirective {
   private authService = inject(AuthService)
   userType = input.required<Permission>({alias:'appAuth'})
+  private templateRef = inject(TemplateRef)
+  private viewContainerRef = inject(ViewContainerRef)
 
   constructor() {
     effect(()=>{
       if(this.authService.activePermission() === this.userType()){
+        this.viewContainerRef.createEmbeddedView(this.templateRef)
         console.log('show');
 
       }else{
         console.log('dont show');
+        this.viewContainerRef.clear()
         
       }
     })
