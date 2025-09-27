9063f2e3818ae391c67d10fae956709139d6d27c
Amirhossein Noori <nouri97dev@gmail.com>
2025-09-27 17:07:28 +0330

    feat:create the element with injection and use that by nativeElement\

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     standalone:true,
@@ -8,6 +8,7 @@ import { Directive, input } from "@angular/core";
 
 export class SafeLinkDirective{
     queryParam = input('myapp' , {alias:'appSafeLink'})
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef)
 
 constructor(){
     console.log('SafeLinkDirective is active!');
@@ -16,8 +17,11 @@ onConfirmLeavePage(event:MouseEvent){
     const wantsToLeave = window.confirm('do you wanna leave?')
 
     if(wantsToLeave){
-        const address= (event.target as HTMLAnchorElement).href;
-        (event.target as HTMLAnchorElement).href = address + '?from='+ this.queryParam()
+        // const address= (event.target as HTMLAnchorElement).href;
+        // (event.target as HTMLAnchorElement).href = address + '?from='+ this.queryParam()
+        const address= this.hostElementRef.nativeElement.href;
+        this.hostElementRef.nativeElement.href = address + '?from='+ this.queryParam()
+
         return
 
     }
